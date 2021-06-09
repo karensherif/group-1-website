aa2d2497febe6aa3ad62b97b2c83fad06dd087c9
karensherif <85609644+karensherif@users.noreply.github.com>
2021-06-09 09:44:37 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var getusername = prompt("hello! who are you? please state your name: ");
+
+console.log (getusername);
+
+
+document.getElementById("karen"). innerText= getusername;
+
+//tooltip
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
